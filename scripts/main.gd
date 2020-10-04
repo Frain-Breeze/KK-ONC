75a1bc74831fdcc41e8c5a1d2b9ee7a21a969138
Frain-Breeze <frainbreezeyt@gmail.com>
2020-10-04 20:49:19 +0200

    little cleanup

--- scripts/main.gd
+++ scripts/main.gd
@@ -20,11 +20,12 @@ var currentRound = -1
 var currentQuestion = -1
 var rng = RandomNumberGenerator.new()
 var inAnswers = false
+var currMode = MODE.QUESTION
 
 func loadFile():
 	print(OS.get_user_data_dir())
 	print(OS.get_executable_path())
-	print(csvFile.open("res://questions/kk-vragen.csv", File.READ))
+	print(csvFile.open("res://questions/loli.csv", File.READ))
 	
 	var inRound = false
 	var tRound = ROUND.new()
@@ -90,58 +91,68 @@ func _ready():
 	next_round()
 	#next_question()
 
-#order of signals:
-#1 advance_round (load new round)
-#2 advance_answer (go to the answer)
-#3 advance_question (go to the next question)
-#4 repeat 2 and 3 until all questions are done, then move to 1
+
+enum MODE{
+	QUESTION,
+	ANSWER,
+	ROUNDEND,
+	RESULTS
+}
+
+func clear_answers():
+	$UI/questions/answer1/text.text = ""
+	$UI/questions/answer2/text.text = ""
+	$UI/questions/answer3/text.text = ""
+	$UI/questions/answer4/text.text = ""
 
 func update_labels():
-	if(content.size() <= currentRound or content[currentRound].questions.size() <= currentQuestion):
-		$UI/questions/answer1/text.text = ""
-		$UI/questions/answer2/text.text = ""
-		$UI/questions/answer3/text.text = ""
-		$UI/questions/answer4/text.text = ""
-		if(currentRound >= content.size()): 
-			$UI/questions/question/text.text = "end of quiz"
-		elif(currentRound >= content.size()-1):
-			$UI/questions/question/text.text = "finale"
-		else:
-			$UI/questions/question/text.text = "moving on to round " + str(currentRound+2)
-		return
+#	if(content.size() <= currentRound or content[currentRound].questions.size() <= currentQuestion):
+#		clear_answers()
+#		if(currentRound >= content.size()): 
+#			$UI/questions/question/text.text = "end of quiz"
+#		elif(currentRound >= content.size()-1):
+#			$UI/questions/question/text.text = "finale"
+#		else:
+#			$UI/questions/question/text.text = "moving on to round " + str(currentRound+2)
+#		return
 	
-	if(!inAnswers):
-		$UI/questions/answer1/text.text = ""
-		$UI/questions/answer2/text.text = ""
-		$UI/questions/answer3/text.text = ""
-		$UI/questions/answer4/text.text = ""
+	if(currMode == MODE.QUESTION):
+		clear_answers()
 		$UI/questions/question/text.text = content[currentRound].questions[currentQuestion].question
-	else:
+	elif(currMode == MODE.ANSWER):
 		$UI/questions/answer1/text.text = content[currentRound].questions[currentQuestion].answer1
 		$UI/questions/answer2/text.text = content[currentRound].questions[currentQuestion].answer2
 		$UI/questions/answer3/text.text = content[currentRound].questions[currentQuestion].answer3
 		$UI/questions/answer4/text.text = content[currentRound].questions[currentQuestion].answer4
 		$UI/questions/question/text.text = ""
+	elif(currMode == MODE.ROUNDEND):
+		clear_answers()
+		$UI/questions/question/text.text = "end of round"
 
 func next_round():
+	currMode = MODE.QUESTION
 	currentQuestion = 0
 	print("NEW ROUND TRIGGERED!")
 	currentRound+=1
 	update_labels()
 
 func next_question():
-	inAnswers = false
+	currMode = MODE.QUESTION
 	currentQuestion+=1
 	print("next question triggered")
 	update_labels()
 
 func show_options():
-	inAnswers = true
+	currMode = MODE.ANSWER
 	print("show options triggered!")
 	update_labels()
 
 func answer_correct(team):
 	print("team " + str(team) + " has correctly answered the question")
+	if content[currentRound].questions.size() <= currentQuestion+1:
+		currMode = MODE.ROUNDEND
+		update_labels()
+		return
 	if(autoAdvanceQuestion):
 		next_question()
 
@@ -149,32 +160,35 @@ func _process(delta):
 	if Input.is_action_just_pressed("show_correct_answer"):
 		print("correct answer: " + str(content[currentRound].questions[currentQuestion].correctAnswer))
 	
-	if inAnswers and Input.is_action_just_pressed("team1_button1") and content[currentRound].questions[currentQuestion%content[currentRound].questions.size()].correctAnswer == 1:
-		answer_correct(1)
-	elif inAnswers and Input.is_action_just_pressed("team1_button2") and content[currentRound].questions[currentQuestion%content[currentRound].questions.size()].correctAnswer == 2:
-		answer_correct(1)
-	elif inAnswers and Input.is_action_just_pressed("team1_button3") and content[currentRound].questions[currentQuestion%content[currentRound].questions.size()].correctAnswer == 3:
-		answer_correct(1)
-	elif inAnswers and Input.is_action_just_pressed("team1_button4") and content[currentRound].questions[currentQuestion%content[currentRound].questions.size()].correctAnswer == 4:
-		answer_correct(1)
+	if currMode == MODE.ANSWER:
+		var CA = content[currentRound].questions[currentQuestion%content[currentRound].questions.size()].correctAnswer
+		if Input.is_action_just_pressed("team1_button1") and CA == 1:
+			answer_correct(1)
+		elif Input.is_action_just_pressed("team1_button2") and CA == 2:
+			answer_correct(1)
+		elif Input.is_action_just_pressed("team1_button3") and CA == 3:
+			answer_correct(1)
+		elif Input.is_action_just_pressed("team1_button4") and CA == 4:
+			answer_correct(1)
+		
+		elif Input.is_action_just_pressed("team2_button1") and CA == 1:
+			answer_correct(2)
+		elif Input.is_action_just_pressed("team2_button2") and CA == 2:
+			answer_correct(2)
+		elif Input.is_action_just_pressed("team2_button3") and CA == 3:
+			answer_correct(2)
+		elif Input.is_action_just_pressed("team2_button4") and CA == 4:
+			answer_correct(2)
 	
-	elif inAnswers and Input.is_action_just_pressed("team2_button1") and content[currentRound].questions[currentQuestion%content[currentRound].questions.size()].correctAnswer == 1:
-		answer_correct(2)
-	elif inAnswers and Input.is_action_just_pressed("team2_button2") and content[currentRound].questions[currentQuestion%content[currentRound].questions.size()].correctAnswer == 2:
-		answer_correct(2)
-	elif inAnswers and Input.is_action_just_pressed("team2_button3") and content[currentRound].questions[currentQuestion%content[currentRound].questions.size()].correctAnswer == 3:
-		answer_correct(2)
-	elif inAnswers and Input.is_action_just_pressed("team2_button4") and content[currentRound].questions[currentQuestion%content[currentRound].questions.size()].correctAnswer == 4:
-		answer_correct(2)
-	
-	
-	
-	if Input.is_action_just_pressed("advance_round"):
-		next_round()
-	
-	if Input.is_action_just_pressed("advance_question"):
-		show_options()
-	
-	if Input.is_action_just_pressed("new_question"):
-		next_question()
-	
+	if Input.is_action_just_pressed("advance"):
+		if(currMode == MODE.ANSWER):
+			next_question()
+		elif(currMode == MODE.ROUNDEND):
+			next_round()
+	else:
+		if Input.is_action_just_pressed("advance_round"):
+			next_round()
+		if Input.is_action_just_pressed("advance_question"):
+			show_options()
+		if Input.is_action_just_pressed("new_question"):
+			next_question()
